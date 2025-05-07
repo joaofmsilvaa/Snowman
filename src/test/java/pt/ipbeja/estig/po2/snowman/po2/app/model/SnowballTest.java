c07e0d7b6b3e839ae777835e7bc9ccd83a5382c2
João Silva <109978656+joaofmsilvaa@users.noreply.github.com>
2025-05-07 17:55:52 +0100

    Create SnowballTest.java

---
+++ src/test/java/pt/ipbeja/estig/po2/snowman/po2/app/model/SnowballTest.java
@@ -0,0 +1,97 @@
+package pt.ipbeja.estig.po2.snowman.po2.app.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pt.ipbeja.estig.po2.snowman.app.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SnowballTest {
+    Monster monster;
+    List<List<PositionContent>> content = new ArrayList<>();
+    List<Snowball> snowballs = new ArrayList<>();
+    BoardModel board;
+
+    int rows = 3;
+    int cols = 3;
+
+    @BeforeEach
+    public void setUp() {
+        monster = new Monster(2,0);
+
+        for (int i = 0; i < rows; i++) {
+            List<PositionContent> row = new ArrayList<>();
+            for (int j = 0; j < cols; j++) {
+                if(i == 0 && j == 1) {
+                    row.add(PositionContent.SNOW);
+                }
+                else{
+                    row.add(PositionContent.NO_SNOW);
+                }
+            }
+            content.add(row);
+        }
+
+        Snowball snowball = new Snowball(1,1, SnowballType.SMALL);
+        snowballs.add(snowball);
+        board = new BoardModel(content, monster, snowballs);
+
+    }
+
+    @Test
+    @DisplayName("Move snowball to the left")
+    void testMoveSnowballToTheLeft(){
+        Snowball snowball = board.snowballInPosition(1, 1);
+        snowball.move(Direction.LEFT, board);
+
+        assertEquals(1, snowball.getRow());
+        assertEquals(0, snowball.getCol());
+
+    }
+
+    @Test
+    @DisplayName("Move the snowball up")
+    void testMoveSnowballToUp(){
+        Snowball snowball = board.snowballInPosition(1, 1);
+        snowball.move(Direction.UP, board);
+
+        assertEquals(0, snowball.getRow());
+        assertEquals(1, snowball.getCol());
+
+    }
+
+    @Test
+    @DisplayName("Create average snowball")
+    void testCreateAverageSnowball(){
+        Snowball snowball = board.snowballInPosition(1, 1);
+        snowball.move(Direction.UP, board);
+
+        assertEquals(0, snowball.getRow());
+        assertEquals(1, snowball.getCol());
+        assertEquals(SnowballType.MID, snowball.getType());
+    }
+
+    @Test
+    @DisplayName("Test invalid snowball move")
+    void testSnowballInvalidMove(){
+        Snowball snowball = board.snowballInPosition(1, 1);
+        boolean status = snowball.move(Direction.UP, board);
+
+        assertEquals(0, snowball.getRow());
+        assertEquals(1, snowball.getCol());
+        assertTrue(status);
+
+        status = snowball.move(Direction.UP, board);
+
+        assertEquals(0, snowball.getRow());
+        assertEquals(1, snowball.getCol());
+        assertFalse(status);
+
+
+    }
+
+}
