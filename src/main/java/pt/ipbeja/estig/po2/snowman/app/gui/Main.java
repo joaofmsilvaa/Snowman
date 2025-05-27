ebca72e6fd81729d27a322aca4136147baa7c486
Traquinices <paulomiguelneves@gmail.com>
2025-05-27 16:48:27 +0100

    fix mainmenu with images

--- src/main/java/pt/ipbeja/estig/po2/snowman/app/gui/Main.java
+++ src/main/java/pt/ipbeja/estig/po2/snowman/app/gui/Main.java
@@ -3,14 +3,16 @@ package pt.ipbeja.estig.po2.snowman.app.gui;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import pt.ipbeja.estig.po2.snowman.app.gui.StartMenu;
+import static pt.ipbeja.estig.po2.snowman.app.gui.StartMenu.show;
 
 public class Main extends Application {
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        StartMenu.show(primaryStage);  // Mostra o menu inicial
+        show(primaryStage);  // Mostra o menu inicial
     }
 }
--- src/main/java/pt/ipbeja/estig/po2/snowman/app/gui/StartMenu.java
+++ src/main/java/pt/ipbeja/estig/po2/snowman/app/gui/StartMenu.java
@@ -3,6 +3,10 @@ package pt.ipbeja.estig.po2.snowman.app.gui;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -10,9 +14,9 @@ import javafx.stage.Stage;
 public class StartMenu {
 
     public static void show(Stage stage) {
-        VBox layout = new VBox(20);
-        layout.setAlignment(Pos.CENTER);
-        layout.setStyle("-fx-background-color: white;");
+
+        Label title = new Label("Snow is hard to build");
+        title.setStyle("-fx-font-size: 36px; -fx-font-weight: bold;");
 
         Button startButton = new Button("Start Game");
         startButton.setStyle("-fx-font-size: 16px; -fx-padding: 10 20;");
@@ -22,8 +26,24 @@ public class StartMenu {
             game.start(stage);  // Inicia o jogo atual
 
         });
+        ImageView monsterView = new ImageView(new Image("/monster1.png"));
+        monsterView.setFitWidth(80);
+        monsterView.setFitHeight(80);
 
-        layout.getChildren().add(startButton);
+        ImageView snowmanView = new ImageView(new Image("/snowman.png"));
+        snowmanView.setFitWidth(80);
+        snowmanView.setFitHeight(80);
+
+        HBox Images = new HBox(40);
+        Images.setAlignment(Pos.CENTER);
+        Images.setStyle("-fx-background-color: white;");
+        Images.getChildren().addAll(monsterView, snowmanView);
+
+        VBox layout = new VBox(40);
+        layout.setAlignment(Pos.CENTER);
+        layout.setStyle("-fx-background-color: white;");
+
+        layout.getChildren().addAll(title, startButton, Images);
 
         Scene scene = new Scene(new StackPane(layout), 600, 600);
         stage.setScene(scene);
