de4be95c47ceb98783abec88cea40ceec9e5815b
João Silva <109978656+joaofmsilvaa@users.noreply.github.com>
2025-05-27 15:19:12 +0100

    Create Big snowball

--- src/test/java/pt/ipbeja/estig/po2/snowman/po2/app/model/BoardModelTest.java
+++ src/test/java/pt/ipbeja/estig/po2/snowman/po2/app/model/BoardModelTest.java
@@ -16,8 +16,8 @@ public class BoardModelTest {
     List<Snowball> snowballs = new ArrayList<>();
     BoardModel board;
 
-    int rows = 3;
-    int cols = 1;
+    int rows = 5;
+    int cols = 5;
 
     @BeforeEach
     public void setUp() {
@@ -26,7 +26,7 @@ public class BoardModelTest {
         for (int i = 0; i < rows; i++) {
             List<PositionContent> row = new ArrayList<>();
             for (int j = 0; j < cols; j++) {
-                if(i == 0) {
+                if(i == 2 && j > 1) {
                     row.add(PositionContent.SNOW);
                 }
                 else{
@@ -36,7 +36,7 @@ public class BoardModelTest {
             content.add(row);
         }
 
-        Snowball snowball = new Snowball(1,0, SnowballType.SMALL);
+        Snowball snowball = new Snowball(2,1, SnowballType.SMALL);
         snowballs.add(snowball);
         board = new BoardModel(content, monster, snowballs);
 
@@ -74,16 +74,16 @@ public class BoardModelTest {
         assertFalse(validMove);
     }
 
-/*    @Test
+    @Test
     void testPositionContent(){
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 PositionContent element = board.getPositionContent(i,j);
-                System.out.print(" | " + element);
+                System.out.print("\t|\t" + element);
             }
-            System.out.println(" |");
+            System.out.println("\t|");
         }
-    }*/
+    }
 
     @Test
     @DisplayName("Move snowball to the left")
@@ -128,6 +128,27 @@ public class BoardModelTest {
         assertEquals(0, snowball.getRow());
         assertEquals(0, snowball.getCol());
         assertEquals(SnowballType.MID, snowball.getType());
+    }
+
+    @Test
+    @DisplayName("Create big snowball")
+    void testCreateBigSnowball(){
+        Snowball snowball = board.snowballInPosition(2, 1);
+        assertEquals(2, snowball.getRow(), "Initial position row");
+        assertEquals(1, snowball.getCol(), "Initial position col");
+        assertEquals(SnowballType.SMALL, snowball.getType());
+
+        board.moveMonster(Direction.RIGHT);
+
+        assertEquals(2, snowball.getRow(), "Second position row");
+        assertEquals(2, snowball.getCol(), "Second position col");
+        assertEquals(SnowballType.MID, snowball.getType());
+
+        board.moveMonster(Direction.RIGHT);
+
+        assertEquals(2, snowball.getRow(), "Third position row");
+        assertEquals(3, snowball.getCol(), "Third position col");
+        assertEquals(SnowballType.BIG, snowball.getType());
     }
 
     @Test
